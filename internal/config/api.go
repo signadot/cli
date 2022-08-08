1af5c0b68150b5a7d20a83ab916618f0ab029a94
Scott Cotton <scott@mindowl.com>
2022-08-08 17:43:58 +0200

    add user agent

--- internal/config/api.go
+++ internal/config/api.go
@@ -3,9 +3,11 @@ package config
 import (
 	"errors"
 	"fmt"
+	"net/http"
 	"net/url"
 
 	oaclient "github.com/go-openapi/runtime/client"
+	"github.com/signadot/cli/internal/buildinfo"
 	"github.com/signadot/cli/internal/hack"
 	"github.com/signadot/go-sdk/client"
 	"github.com/spf13/viper"
@@ -50,7 +52,25 @@ func (a *Api) InitAPIConfig() error {
 	transport.DefaultAuthentication = oaclient.APIKeyAuth("signadot-api-key", "header", apiKey)
 	transport.SetDebug(a.Debug)
 
+	// Add User-Agent to every request
+	transport.Transport = setUserAgent(transport.Transport,
+		fmt.Sprintf("signadot-cli:%s", buildinfo.Version))
+
 	a.Client = client.New(hack.FixAPIErrors(transport), nil)
 
 	return nil
+}
+
+func setUserAgent(t http.RoundTripper, ua string) http.RoundTripper {
+	return &userAgent{t, ua}
+}
+
+type userAgent struct {
+	inner http.RoundTripper
+	agent string
+}
+
+func (ua *userAgent) RoundTrip(r *http.Request) (*http.Response, error) {
+	r.Header.Set("User-Agent", ua.agent)
+	return ua.inner.RoundTrip(r)
 }
