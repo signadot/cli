194da6459e90dc69ef6fa6c7fccbdc87bebb38e7
Scott Cotton <scott@mindowl.com>
2022-09-02 16:53:40 +0200

    reduce-wait-timeout

--- internal/config/sandbox.go
+++ internal/config/sandbox.go
@@ -23,7 +23,7 @@ type SandboxApply struct {
 func (c *SandboxApply) AddFlags(cmd *cobra.Command) {
 	cmd.Flags().StringVarP(&c.Filename, "filename", "f", "", "YAML or JSON file containing the sandbox creation request")
 	cmd.Flags().BoolVar(&c.Wait, "wait", true, "wait for the sandbox status to be Ready before returning")
-	cmd.Flags().DurationVar(&c.WaitTimeout, "wait-timeout", 5*time.Minute, "timeout when waiting for the sandbox to be Ready")
+	cmd.Flags().DurationVar(&c.WaitTimeout, "wait-timeout", 3*time.Minute, "timeout when waiting for the sandbox to be Ready")
 	cmd.MarkFlagRequired("filename")
 	cmd.Flags().Var(&c.TemplateVals, "set", "--set var=val")
 }
