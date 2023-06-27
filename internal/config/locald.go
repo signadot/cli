b14df1a163b7275fc2ebdb81ff66e44b746e6287
Scott Cotton <scott@mindowl.com>
2023-06-27 17:50:39 +0200

    CR: flag name improvement

--- internal/config/locald.go
+++ internal/config/locald.go
@@ -18,5 +18,5 @@ func (c *LocalDaemon) AddFlags(cmd *cobra.Command) {
 	cmd.Flags().BoolVar(&c.RunUnpriveleged, "sandbox-manager", false, "run without root priveleges")
 	cmd.Flags().StringVar(&c.Cluster, "cluster", "", "signadot cluster name")
 	cmd.Flags().Uint16Var(&c.Port, "api-port", 6666, "api service port")
-	cmd.Flags().IntVar(&c.UID, "org-uid", -1, "original uid")
+	cmd.Flags().IntVar(&c.UID, "uid", -1, "original uid of caller (if run as root)")
 }
