1eff485f035c450d15bbd75b135eb1e8aaf2419f
Scott Cotton <scott@mindowl.com>
2023-07-14 00:02:14 +0200

    CR: don't use pid monitors

--- internal/command/local/disconnect.go
+++ internal/command/local/disconnect.go
@@ -13,7 +13,6 @@ import (
 	"github.com/signadot/cli/internal/utils/system"
 	"github.com/signadot/libconnect/common/processes"
 	"github.com/spf13/cobra"
-	"golang.org/x/exp/slog"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -45,10 +44,13 @@ func runDisconnect(cfg *config.LocalDisconnect, args []string) error {
 	ticker := time.NewTicker(time.Second / 10)
 	defer ticker.Stop()
 	wasRunning := false
-	for !runState.isDone() {
+	for {
 		wasRunning = runState.init(signadotDir) || wasRunning
 		err := runState.tryKill()
 		_ = err
+		if runState.isDone() {
+			break
+		}
 		<-ticker.C
 	}
 
@@ -59,28 +61,21 @@ func runDisconnect(cfg *config.LocalDisconnect, args []string) error {
 	return fmt.Errorf("signadot was not connected")
 }
 
-// process monitor result for one of rootmanager, sandbox manager
-type mgrRunState struct {
-	C   <-chan struct{}
-	PID int
-	E   error
-}
-
 // we have a sandbox manager and a root manager to stop, and may be
 // run in unprivileged mode, without root manager, so
 // for stopping them we keep track of both.
 type runState struct {
-	Root    mgrRunState
-	NotRoot mgrRunState
+	RootPIDFilePresent    bool
+	NotRootPIDFilePresent bool
 }
 
 // we are done when there are no more pid files
 func (rs *runState) isDone() bool {
-	return os.IsNotExist(rs.Root.E) && os.IsNotExist(rs.NotRoot.E)
+	return !rs.RootPIDFilePresent && !rs.NotRootPIDFilePresent
 }
 
 func (rs *runState) tryKill() error {
-	if rs.Root.C != nil {
+	if rs.RootPIDFilePresent {
 		// Establish a connection with sandbox manager
 		grpcConn, err := grpc.Dial("127.0.0.1:6667", grpc.WithTransportCredentials(insecure.NewCredentials()))
 		if err != nil {
@@ -97,7 +92,7 @@ func (rs *runState) tryKill() error {
 		}
 		return nil
 	}
-	if rs.NotRoot.C != nil {
+	if rs.NotRootPIDFilePresent {
 		// Establish a connection with root manager
 		grpcConn, err := grpc.Dial("127.0.0.1:6666", grpc.WithTransportCredentials(insecure.NewCredentials()))
 		if err != nil {
@@ -121,8 +116,9 @@ func (rs *runState) init(signadotDir string) bool {
 	sbmPIDFile := filepath.Join(signadotDir, config.SandboxManagerPIDFile)
 	processes.CleanPIDFile(rmPIDFile)
 	processes.CleanPIDFile(sbmPIDFile)
-
-	rs.Root.C, rs.Root.PID, rs.Root.E = processes.MonitorPID(rmPIDFile, slog.Default())
-	rs.NotRoot.C, rs.NotRoot.PID, rs.NotRoot.E = processes.MonitorPID(sbmPIDFile, slog.Default())
-	return rs.Root.PID != 0 || rs.NotRoot.PID != 0
+	_, err := os.Stat(rmPIDFile)
+	rs.RootPIDFilePresent = err == nil || !os.IsNotExist(err)
+	_, err = os.Stat(sbmPIDFile)
+	rs.NotRootPIDFilePresent = err == nil || !os.IsNotExist(err)
+	return rs.RootPIDFilePresent || rs.NotRootPIDFilePresent
 }
