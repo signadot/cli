a08e8f96b19c4ba083cc5aec14d129b7e2fcdfd7
Scott Cotton <scott@mindowl.com>
2023-10-25 15:21:31 +0200

    add last sync to cluster rows

--- internal/command/cluster/printers.go
+++ internal/command/cluster/printers.go
@@ -8,19 +8,30 @@ import (
 )
 
 type clusterRow struct {
-	Name    string `sdtab:"NAME"`
-	Created string `sdtab:"CREATED"`
-	Version string `sdtab:"OPERATOR VERSION"`
+	Name     string `sdtab:"NAME"`
+	Created  string `sdtab:"CREATED"`
+	Version  string `sdtab:"OPERATOR VERSION"`
+	LastSync string `sdtab:"LAST SYNC"`
 }
 
 func printClusterTable(out io.Writer, clusters []*models.Cluster) error {
 	t := sdtab.New[clusterRow](out)
 	t.AddHeader()
 	for _, cluster := range clusters {
+		lastSync := "-"
+		if cluster.SyncStatus != nil {
+			syncStat := cluster.SyncStatus
+			if syncStat.LastSync != nil {
+				if syncStat.LastSync.Time != "" {
+					lastSync = syncStat.LastSync.Time
+				}
+			}
+		}
 		t.AddRow(clusterRow{
-			Name:    cluster.Name,
-			Created: cluster.CreatedAt,
-			Version: cluster.Operator.Version,
+			Name:     cluster.Name,
+			Created:  cluster.CreatedAt,
+			Version:  cluster.Operator.Version,
+			LastSync: lastSync,
 		})
 	}
 	return t.Flush()
