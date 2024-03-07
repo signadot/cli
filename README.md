d061246226f4d223e3e155738ecf4b2829ea1613
Daniel De Vera <118383315+daniel-de-vera@users.noreply.github.com>
2024-03-07 12:27:06 -0300

    Addition of an installation script (#117)
    * Addition of an installation script
    
    * Add some checks in install script

--- README.md
+++ README.md
@@ -11,6 +11,16 @@ NOTE: Starting next release, and hence in the main branch, this repository has a
 on a private repository, and hence building or running from source will not work.
 Previous releases should continue to work.
 
+## Install
+
+To install the CLI, run:
+
+```sh
+curl -sSLf https://raw.githubusercontent.com/signadot/cli/main/scripts/install.sh | sh
+```
+
+By default, the script will install the latest version at `/usr/local/bin/signadot`. The target version can be selected by setting the SIGNADOT_CLI_VERSION variable, while you can specify the install directory with `SIGNADOT_CLI_PATH`.
+
 ## Build
 
 To build the CLI from source, such as to test changes, you'll need Go 1.18+.
---
+++ scripts/install.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+#
+# Signadot CLI installer
+#
+# Usage:
+#   curl -sSLf https://raw.githubusercontent.com/signadot/cli/main/scripts/install.sh | sh
+
+echo "Installing Signadot CLI"
+
+if [ ! -z "${DEBUG}" ];
+then set -x
+fi
+
+if ! command -v jq > /dev/null 2>&1; then
+    echo 'Error: jq is not installed and is required.' >&2
+    exit 1
+fi
+if ! command -v curl > /dev/null 2>&1; then
+    echo 'Error: curl is not installed and is required.' >&2
+    exit 1
+fi
+
+_detect_arch() {
+    case $(uname -m) in
+    amd64|x86_64) echo "amd64"
+    ;;
+    arm64|aarch64) echo "arm64"
+    ;;
+    *) echo "Unsupported processor architecture";
+    return 1
+    ;;
+     esac
+}
+
+_detect_os(){
+    case $(uname) in
+    Linux) echo "linux"
+    ;;
+    Darwin) echo "darwin"
+    ;;
+    *) echo "Unsupported os";
+    return 1
+    ;;
+    esac
+}
+
+_download_url() {
+  local arch
+  local os
+  local tag
+  local version
+
+  arch="$(_detect_arch)"
+  os="$(_detect_os)"
+  if [ -z "$SIGNADOT_CLI_VERSION" ]; then
+      tag="$(
+        curl -s "https://api.github.com/repos/signadot/cli/releases/latest" \
+        2>/dev/null \
+        | jq -r '.tag_name' \
+      )"
+  else
+    tag="$SIGNADOT_CLI_VERSION"
+  fi
+
+  echo "https://github.com/signadot/cli/releases/download/${tag}/signadot-cli_${os}_${arch}.tar.gz"
+}
+
+echo "Downloading signadot binary from URL: $(_download_url)"
+curl -sSLf "$(_download_url)" > signadot-cli.tar.gz
+
+tar -xzf signadot-cli.tar.gz signadot
+rm signadot-cli.tar.gz
+
+if [ -z "$SIGNADOT_CLI_PATH" ]; then
+    cli_path="/usr/local/bin/signadot"
+    # use sudo if it exists
+    if command -v sudo > /dev/null 2>&1; then
+        sudo mv signadot $cli_path
+    else
+        mv signadot $cli_path
+    fi
+else
+    cli_path="${SIGNADOT_CLI_PATH%/}" # remove traling / if present
+    cli_path="$cli_path/signadot"
+    mv signadot $cli_path
+fi
+echo "signadot installed in:"
+echo "- $cli_path"
