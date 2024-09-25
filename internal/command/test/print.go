bd3919b787855a593975f722cfa37b6b394915be
Scott Cotton <scott@mindowl.com>
2024-09-25 09:00:30 +0200

    add "test details" output

--- internal/command/test/print.go
+++ internal/command/test/print.go
@@ -3,17 +3,18 @@ package test
 import (
 	"fmt"
 	"io"
+	"text/tabwriter"
 
 	"github.com/signadot/cli/internal/config"
 	"github.com/signadot/cli/internal/print"
+	"github.com/signadot/cli/internal/utils"
 	"github.com/signadot/go-sdk/models"
 )
 
 func printTest(oFmt config.OutputFormat, w io.Writer, t *models.Test) error {
 	switch oFmt {
 	case config.OutputFormatDefault:
-		// TODO
-		return nil
+		return printTestDetails(w, t)
 	case config.OutputFormatJSON:
 		return print.RawJSON(w, t)
 	case config.OutputFormatYAML:
@@ -21,4 +22,13 @@ func printTest(oFmt config.OutputFormat, w io.Writer, t *models.Test) error {
 	default:
 		return fmt.Errorf("unsupported output format: %q", oFmt)
 	}
+}
+
+func printTestDetails(out io.Writer, t *models.Test) error {
+	tw := tabwriter.NewWriter(out, 0, 0, 3, ' ', 0)
+
+	fmt.Fprintf(tw, "Name:\t%s\n", t.Name)
+	fmt.Fprintf(tw, "Created:\t%s\n", utils.FormatTimestamp(t.CreatedAt))
+	fmt.Fprintf(tw, "Updated:\t%s\n", utils.FormatTimestamp(t.UpdatedAt))
+	return tw.Flush()
 }
