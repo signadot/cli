a692159ac2654c888991856e14ff7dd74aea71d6
dorozco <orozco.davidj02@gmail.com>
2025-09-30 11:29:06 -0500

    feat: add helpers in sandbox builder that allows to set machine id and others fields

--- internal/builder/sandbox.go
+++ internal/builder/sandbox.go
@@ -4,14 +4,44 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/signadot/cli/internal/utils/system"
 	"github.com/signadot/go-sdk/models"
 )
 
 type SandboxBuilder struct {
-	internal models.Sandbox
+	internal              models.Sandbox
+	err                   error
+	lastAddedOverrideName *string
 }
 
 type Option func(SandboxBuilder) SandboxBuilder
+
+func NewSandboxBuilder() *SandboxBuilder {
+	return &SandboxBuilder{
+		internal: models.Sandbox{},
+	}
+}
+
+// setError sets the error if it hasn't been set already (first error wins)
+func (sb *SandboxBuilder) setError(err error) *SandboxBuilder {
+	if sb.err == nil && err != nil {
+		sb.err = err
+	}
+	return sb
+}
+
+// checkError returns true if there's already an error, false otherwise
+func (sb *SandboxBuilder) checkError() bool {
+	return sb.err != nil
+}
+
+// withError executes a function that can return an error, and sets the error if it fails
+func (sb *SandboxBuilder) withError(fn func() error) *SandboxBuilder {
+	if sb.checkError() {
+		return sb
+	}
+	return sb.setError(fn())
+}
 
 func BuildSandbox(name string, opts ...Option) *SandboxBuilder {
 	sb := SandboxBuilder{
@@ -27,8 +57,18 @@ func BuildSandbox(name string, opts ...Option) *SandboxBuilder {
 	return &sb
 }
 
-func (sb *SandboxBuilder) Build() models.Sandbox {
-	return sb.internal
+func (sb *SandboxBuilder) Build() (models.Sandbox, error) {
+	return sb.internal, sb.err
+}
+
+// Error returns the current error, if any
+func (sb *SandboxBuilder) Error() error {
+	return sb.err
+}
+
+// HasError returns true if there's an error, false otherwise
+func (sb *SandboxBuilder) HasError() bool {
+	return sb.checkError()
 }
 
 type MiddlewareName string
@@ -37,7 +77,11 @@ const (
 	AltflowMiddleware MiddlewareName = "altflow"
 )
 
-func (sb *SandboxBuilder) AddOverrideMiddleware(worklaodPort int64, toLocal string, workloadNames ...string) string {
+func (sb *SandboxBuilder) AddOverrideMiddleware(worklaodPort int64, toLocal string, workloadNames ...string) *SandboxBuilder {
+	if sb.checkError() {
+		return sb
+	}
+
 	hostArg := &models.SandboxesArgument{
 		Name:  "altHost",
 		Value: sb.internal.Name,
@@ -78,7 +122,83 @@ func (sb *SandboxBuilder) AddOverrideMiddleware(worklaodPort int64, toLocal stri
 
 	spec.Routing.Forwards = append(spec.Routing.Forwards, forwardRouting)
 
-	return forwardName
+	sb.lastAddedOverrideName = &forwardName
+
+	return sb
+}
+
+func (sb *SandboxBuilder) DeleteOverrideMiddleware(overrideName string) *SandboxBuilder {
+	if sb.checkError() {
+		return sb
+	}
+
+	spec := sb.internal.Spec
+
+	// Delete routing.forward by name
+	if spec.Routing == nil {
+		return sb
+	}
+
+	spec.Routing.Forwards = removeForwardByName(spec.Routing.Forwards, overrideName)
+
+	// Delete match in the middleware when the args.altHost is the same as the overrideName
+	if spec.Middleware == nil {
+		return sb
+	}
+
+	spec.Middleware = removeMiddleareByValueFrom(spec.Middleware, overrideName)
+
+	return sb
+}
+
+func removeForwardByName(forwards []*models.SandboxesForward, name string) []*models.SandboxesForward {
+	newForwards := make([]*models.SandboxesForward, 0)
+	for _, forward := range forwards {
+		if forward.Name != name {
+			newForwards = append(newForwards, forward)
+		}
+	}
+	return newForwards
+}
+
+func removeMiddleareByValueFrom(middlewares []*models.SandboxesMiddleware, value string) []*models.SandboxesMiddleware {
+	newMiddlewares := make([]*models.SandboxesMiddleware, 0)
+
+	getAltHostArg := func(middleware *models.SandboxesMiddleware) *models.SandboxesArgument {
+		for _, arg := range middleware.Args {
+			if arg.Name == "altHost" {
+				return arg
+			}
+		}
+		return nil
+	}
+
+	for _, middleware := range middlewares {
+		altHostArg := getAltHostArg(middleware)
+
+		// TODO: Replace the value with "valueFrom" argument
+		if altHostArg != nil && altHostArg.Value == value {
+			continue
+		}
+
+		newMiddlewares = append(newMiddlewares, middleware)
+	}
+	return newMiddlewares
+}
+
+func (sb *SandboxBuilder) GetLastAddedOverrideName() *string {
+	return sb.lastAddedOverrideName
+}
+
+func (sb *SandboxBuilder) SetMachineID() *SandboxBuilder {
+	return sb.withError(func() error {
+		machineID, err := system.GetMachineID()
+		if err != nil {
+			return err
+		}
+		sb.internal.Spec.LocalMachineID = machineID
+		return nil
+	})
 }
 
 func WithData(data models.Sandbox) Option {
