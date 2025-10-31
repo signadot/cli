45b2139f572240fa9c26893c6b4b5c7384d4d739
dorozco <orozco.davidj02@gmail.com>
2025-10-31 14:46:01 -0500

    fix right to show last focused on following mode

--- internal/tui/views/trafficwatch/main_view.go
+++ internal/tui/views/trafficwatch/main_view.go
@@ -335,22 +335,19 @@ func (m *MainView) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	}
 
 	// Update focused pane
-	var cmd tea.Cmd
 	if m.state == StateWithData {
 		m.rightPane.SetFocused(m.focus == "right")
 
-		switch m.focus {
-		case "left":
-			_, cmd = m.leftPane.Update(msg)
-		case "right":
-			_, cmd = m.rightPane.Update(msg)
-		}
+		_, cmd := m.leftPane.Update(msg)
+		cmds = append(cmds, cmd)
+
+		_, cmd = m.rightPane.Update(msg)
 	}
 
 	if m.state == StateLogs {
-		_, cmd = m.logsView.Update(msg)
+		_, cmd := m.logsView.Update(msg)
+		cmds = append(cmds, cmd)
 	}
-	cmds = append(cmds, cmd)
 
 	return m, tea.Batch(cmds...)
 }
