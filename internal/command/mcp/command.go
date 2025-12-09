b853b993ae1e4f6f0e6c62b36245de1a631a687b
Daniel De Vera <daniel@signadot.com>
2025-12-09 14:44:07 -0300

    Rename CLI command to  signadot mcp

--- internal/command/mcp/command.go
+++ internal/command/mcp/command.go
@@ -1,21 +1,37 @@
 package mcp
 
 import (
+	"context"
+
 	"github.com/signadot/cli/internal/config"
+	"github.com/signadot/cli/internal/mcp"
 	"github.com/spf13/cobra"
 )
 
 func New(api *config.API) *cobra.Command {
-	cfg := &config.MCP{API: api}
+	cfg := &config.MCP{
+		API: api,
+	}
 	cmd := &cobra.Command{
 		Use:   "mcp",
-		Short: "MCP server commands",
+		Short: "Run MCP server",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return run(cmd.Context(), cfg)
+		},
+	}
+	cfg.AddFlags(cmd)
+	return cmd
+}
+
+func run(ctx context.Context, cfg *config.MCP) error {
+	// initialize api config
+	if err := cfg.API.InitUnauthAPIConfig(); err != nil {
+		return err
 	}
 
-	// Subcommands
-	cmd.AddCommand(
-		newRun(cfg),
-	)
+	// create mcp server
+	srv := mcp.NewServer(cfg)
 
-	return cmd
+	// run mcp server
+	return srv.Run(ctx)
 }
